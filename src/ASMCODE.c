5e0b0c11581b323eba90af55c395120a53eb4fbf
Harsh Pandey <harshxvinod0331@gmail.com>
2026-08-04 14:51:23 +0530

    refactor assembly program

--- src/ASMCODE.c
+++ src/ASMCODE.c
@@ -1,86 +1,84 @@
 #include"TACcode.h"
-#include"optimizer.h"
 #include"ASMCODE.h"
 
-char* SP1 = "SP";
-
-REG_MAP regmap[100];
-int reg_map_count = 0;
+#define MAX_INSTRUCTION_LENGTH 20
 
 const char* regs_name[] = {"R0" , "R1" , "R2" , "R3"};
 
-int reg_free[] = {1,1,1,1};
-
-char STACK[1000][50];
-int SPTR = 0;
-
-void stack_push(char *value){
-    strcpy(STACK[SPTR] , value);
-    SPTR++;
+void stack_push(ASM_STACK *stack , char *value){
+    strcpy(stack->STACK[stack->top] , value);
+    stack->top++;
 }
 
-char *stack_pop(){
-    SPTR--;
-    return STACK[SPTR];
+char *stack_pop(ASM_STACK *stack){
+    stack->top--;
+    return stack->STACK[stack->top];
 }
 
 
-int alloc_reg(){
+int alloc_reg(REGISTER *registers){
     for(int i=0 ; i<REG_COUNT ; i++){
-        if(reg_free[i] == 1){
-            reg_free[i] = 0;
+        if(registers->reg_free[i] == 1){
+            registers->reg_free[i] = 0;
             return i;
         }
     }
-    printf("ERROR!, no free register.\n");
+    //printf("ERROR!, no free register.\n");
     return -1;
 }
 
-void free_reg(int index){
-    reg_free[index] = 1;
+void free_reg(int index , REGISTER *registers){
+    registers->reg_free[index] = 1;
+    for(int i=0 ; i<registers->register_count ; i++){
+        if(registers->register_map[i].register_index == index){
+            registers->register_map[i].temp[0] = '\0';
+            registers->register_map[i].register_index = -1;
+            break;
+        }
+    }
 }
 
-int find_reg(char* temp){
-    for(int i=0 ; i<reg_map_count ; i++){
-        if(strcmp(regmap[i].temp , temp)==0){
-            return regmap[i].reg_index;
+int find_reg(const char* temp , const REGISTER *registers){
+    for(int i=0 ; i<registers->register_count ; i++){
+        if(strcmp(registers->register_map[i].temp , temp)==0){
+            return registers->register_map[i].register_index;
         }
     }
     return -1;
 }
 
-void Generate_code(){
+void Generate_code(TACProgram *program , REGISTER *registers){
     printf("\n-----------ASSEMBLY CODE--------------\n");
 
-    for(int i=0 ; i<tac_count ; i++){
-        if(tac_table[i].is_dead) continue;
+    for(int i=0 ; i<program->tac_count ; i++){
+        if(program->code[i].is_dead) continue;
 
-        switch(tac_table[i].type){
+        switch(program->code[i].type){
             case TAC_ASSIGN: {
-                char* result = tac_table[i].result;
-                char* op1 = tac_table[i].op1;
-                char* op2 = tac_table[i].op2;
-                char* opr = tac_table[i].opr;
+                char* result = program->code[i].result;
+                char* op1 = program->code[i].op1;
+                char* op2 = program->code[i].op2;
+                char* opr = program->code[i].opr;
 
                 //printf("RESULT FOR ASM = %s\n",result);
                 //printf("OP1 FOR ASM = %s\n",op1);
                 //printf("OP2 FOR ASM = %s\n",op2);
                 //printf("OPR FOR ASM = %s\n",opr);
-                //printf("DEREFERENCE WRITE = %d\n",tac_table[i].is_deref_write);
-                //printf("addr of = %d\n",tac_table[i].is_addr);
+                //printf("DEREFERENCE WRITE = %d\n",program->code[i].is_deref_write);
+                //printf("addr of = %d\n",program->code[i].is_addr);
                 if(strcmp(op2 , "")==0){
-                int reg = find_reg(op1);
-                if(reg != -1 && isalpha(tac_table[i].op1[0])){
+                int reg = find_reg(op1 , registers);
+                if(reg != -1 && isalpha(program->code[i].op1[0])){
                     //printf("if.\n");
                     printf("MOV %s, %s\n", result , regs_name[reg]);
-                    free_reg(reg);
+                    free_reg(reg , registers);
                 }
-                else if(tac_table[i].is_addr == 1 && strcmp(tac_table[i].opr , "&") == 0){
+                else if(program->code[i].is_addr == 1 && strcmp(program->code[i].opr , "&") == 0){
                     //printf("else if for addr.\n");
                     printf("MOV %s , &%s\n",result , op1);
-                    free_reg(reg);
+                    free_reg(reg , registers);
                 }
-                else if(tac_table[i].is_deref_write == 1 && strcmp(tac_table[i].opr , "*") == 0){
+                else if(program->code[i].is_deref_write == 1 && strcmp(program->code[i].opr , "*") == 0){
                     //printf("else if\n");
                     printf("MOV *%s , ",result);
                     printf("%s\n",op1);
@@ -92,22 +90,27 @@ void Generate_code(){
                 continue;
             }
 
-            int reg = alloc_reg();
+            int reg = alloc_reg(registers);
 
-            strcpy(regmap[reg_map_count].temp,result);
-            regmap[reg_map_count].reg_index = reg;
-            reg_map_count++;
+            if(reg == -1){
+                printf("ERROR , register can not be allocated.\n");
+                return;
+            }
 
-            int op1_reg = find_reg(op1);
+            strcpy(registers->register_map[registers->register_count].temp , result);
+            registers->register_map[registers->register_count].register_index = reg;
+            registers->register_count++;
+
+            int op1_reg = find_reg(op1 , registers);
 
             if(op1_reg != -1){
                 printf("MOV %s, %s\n",regs_name[reg] , regs_name[op1_reg]);
-                free_reg(op1_reg);
+                free_reg(op1_reg , registers);
             }else{
                 printf("MOV %s, %s\n",regs_name[reg] , op1);
             }
 
-            char instr[10];
+            char instr[MAX_INSTRUCTION_LENGTH];
             if(strcmp(opr , "+")==0) strcpy(instr , "ADD");
             else if(strcmp(opr , "-")==0) strcpy(instr , "SUB");
             else if(strcmp(opr , "*")==0) strcpy(instr , "MUL");
@@ -115,11 +118,11 @@ void Generate_code(){
             else if(strcmp(opr , "%")==0) strcpy(instr , "MOD");
             else if(strcmp(opr , "=")==0) strcpy(instr , "MOV");
         
-            int op2_reg = find_reg(op2);
+            int op2_reg = find_reg(op2 , registers);
 
             if(op2_reg != -1){
                 printf("%s %s, %s\n",instr , regs_name[reg] , regs_name[op2_reg]);
-                free_reg(op2_reg);
+                free_reg(op2_reg , registers);
             }else{
                 printf("%s %s, %s\n",instr , regs_name[reg] , op2);
             }
@@ -127,45 +130,45 @@ void Generate_code(){
             break;
             }
             case TAC_IF_GOTO:
-                printf("CMP %s , %s\n" , tac_table[i].op1 , tac_table[i].op2);
+                printf("CMP %s , %s\n" , program->code[i].op1 , program->code[i].op2);
 
-                if(strcmp(tac_table[i].opr , "<") == 0){
-                    printf("JL %s \n", tac_table[i].label);
+                if(strcmp(program->code[i].opr , "<") == 0){
+                    printf("JL %s \n", program->code[i].label);
                 }
-                else if(strcmp(tac_table[i].opr , ">") == 0){
-                    printf("JG %s \n", tac_table[i].label);
+                else if(strcmp(program->code[i].opr , ">") == 0){
+                    printf("JG %s \n", program->code[i].label);
                 }
-                else if(strcmp(tac_table[i].opr , "==") == 0){
-                    printf("JE %s \n", tac_table[i].label);
+                else if(strcmp(program->code[i].opr , "==") == 0){
+                    printf("JE %s \n", program->code[i].label);
                 }
-                else if(strcmp(tac_table[i].opr , "!=") == 0){
-                    printf("JNE %s \n", tac_table[i].label);
+                else if(strcmp(program->code[i].opr , "!=") == 0){
+                    printf("JNE %s \n", program->code[i].label);
                 }
-                else if(strcmp(tac_table[i].opr , "<=") == 0){
-                    printf("JLE %s \n", tac_table[i].label);
+                else if(strcmp(program->code[i].opr , "<=") == 0){
+                    printf("JLE %s \n", program->code[i].label);
                 }
-                else if(strcmp(tac_table[i].opr , ">=") == 0){
-                    printf("JGE %s \n", tac_table[i].label);
+                else if(strcmp(program->code[i].opr , ">=") == 0){
+                    printf("JGE %s \n", program->code[i].label);
                 }
                 break;
             case TAC_GOTO:
-                printf("JMP %s \n",tac_table[i].label);
+                printf("JMP %s \n",program->code[i].label);
                 break;
             case TAC_LABEL:
-                printf("%s:\n", tac_table[i].label);
+                printf("%s:\n", program->code[i].label);
                 break;
 
             case PARAM:
-                printf("PUSH %s\n",tac_table[i].op1);
+                printf("PUSH %s\n",program->code[i].op1);
                 break;
 
             case FUNC_CALL:
-                printf("CALL %s\n",tac_table[i].op1);
+                printf("CALL %s\n",program->code[i].op1);
                 printf("MOV RETVAL , R0\n");
                 break;
 
             case RETURN:
-                printf("MOV R0 , %s\n",tac_table[i].op1);
+                printf("MOV R0 , %s\n",program->code[i].op1);
                 printf("RET\n");
                 break;
         }
--- src/ASMCODE.h
+++ src/ASMCODE.h
@@ -1,27 +1,38 @@
 #ifndef ASMCODE_H
 #define ASMCODE_H
 
-#define REG_COUNT 4
+#include"TACcode.h"
 
-typedef struct reg_map{
+#define REG_COUNT 4
+#define MAX_REGISTER_MAP 200
+#define MAX_STACK_ROW 1000
+#define MAX_STACK_COLUMN 50
+typedef struct REG_MAP{
     char temp[50];
-    int reg_index;
+    int register_index;
 }REG_MAP;
 
+typedef struct{
+    REG_MAP register_map[MAX_REGISTER_MAP];
+    int register_count;
+    int reg_free[REG_COUNT];
+}REGISTER;
 
-extern REG_MAP regmap[100];
-extern int reg_map_count;
+typedef struct ASM_STACK{
+    char STACK[MAX_STACK_ROW][MAX_STACK_COLUMN];
+    int top;
+}ASM_STACK;
 
-int alloc_reg();
+int alloc_reg(REGISTER *registers);
 
-void stack_push(char *value);
+void stack_push(ASM_STACK *stack , char *value);
 
-char *stack_pop();
+char *stack_pop(ASM_STACK *stack);
 
-void free_reg(int index);
+void free_reg(int index , REGISTER *registers);
 
-int find_reg(char* temp);
+int find_reg(const char* temp , const REGISTER *registers);
 
-void Generate_code();
+void Generate_code(TACProgram *program , REGISTER *registers);
 
 #endif // ASMCODE_H
